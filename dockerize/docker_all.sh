067512e9c763a632a6d63c66b85ea169e994283b
Michael Marshall <michaelmarshall220@gmail.com>
2022-08-16 18:40:08 -0500

    updated docker all with new db api

--- dockerize/docker_all.sh
+++ dockerize/docker_all.sh
@@ -14,6 +14,12 @@ cd ../2b-dataset-api-iso-639-2
 ./gradlew bootJar
 docker build -t etenlab/2b-dataset-api-iso-639-2 .
 
+# database api
+echo "\n\n2b-database-api"
+cd ../2b-database-api
+./gradlew bootJar
+docker build -t etenlab/2b-database-api .
+
 cd ..
 
 echo "\n\nfinished. you should be ready to run docker compose.\n\n"
