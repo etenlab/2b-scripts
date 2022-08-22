729060f9564ea11859c3be72171d9fbea42d6dad
Michael Marshall <michaelmarshall220@gmail.com>
2022-08-22 17:52:17 -0500

    added script to load all data

---
+++ dataset-scripts/gimme.sh
@@ -0,0 +1,6 @@
+python3 scripts/dataset-scripts/parse-639-2-html.py
+python3 scripts/dataset-scripts/parse-639-3-macrolanguages-tab.py
+python3 scripts/dataset-scripts/parse-639-3-name-index-tab.py
+python3 scripts/dataset-scripts/parse-639-3-Retirements-tab.py
+python3 scripts/dataset-scripts/parse-639-3-tab.py
+python3 scripts/dataset-scripts/parse-639-5-html.py
