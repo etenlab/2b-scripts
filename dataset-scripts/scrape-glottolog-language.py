5936783d5dbe6cfe81c63cab7b04069046657ec9
aslambabu <aslambabu@gmail.com>
2022-09-21 08:00:53 +0530

    scrape script cleaning and chrome driver doc

--- dataset-scripts/scrape-glottolog-language.py
+++ dataset-scripts/scrape-glottolog-language.py
@@ -17,49 +17,40 @@ options.headless = True
 
 file = open('data-glottolog-org-language.csv', 'w+', newline='')
 
+# Download chromedriver and set the PATH environment variable or hardcode the executable_path in webdriver.Chrome function
+# For more details about setting up chrome driver, see this documentation. https://www.selenium.dev/documentation/webdriver/getting_started/install_drivers/
 chrome_options = webdriver.ChromeOptions()
 # chrome_options.add_argument("--headless")
 # chrome_options.add_argument("--disable-gpu")
 driver = webdriver.Chrome(
     executable_path='/home/aslam/programs/chromedriver_linux64/chromedriver', chrome_options=chrome_options)
 
-# driver = webdriver.Chrome(
-#     '/home/aslam/programs/chromedriver_linux64/chromedriver', options=options)
 
 URL = "https://glottolog.org/glottolog/language"
 driver.get(URL)
 
 time.sleep(3)
-# print(driver.title)
-# driver.implicitly_wait(15)
 soup = BeautifulSoup(driver.page_source, 'lxml')
 
 dataTable = soup.find(id="Families")
 page = 1
 thead = dataTable.find_all("thead")
 tbody = dataTable.find_all("tbody")
-# print(tbody)
 titleCols = thead[0].find_all("tr")[0].find_all('th')
 titles = [ele.text.strip() for ele in titleCols]
 nextPage = True
 with file:
-    #writer = csv.DictWriter(file, fieldnames=titles)
-    # writer.writeheader()
     writer = csv.writer(file)
     writer.writerow(titles)
-    # write.writerows(titles)
     dataRows = tbody[0].find_all("tr")
-    # print(dataRows)
 
     for dataRow in dataRows:
         dataCols = dataRow.find_all('td')
         dataCols = [ele.text.strip() for ele in dataCols]
-
         writer.writerow(dataCols)
 
     print("Page " + str(page) + ":Data Fetched")
     page = page + 1
-
     elm1 = driver.find_element_by_css_selector("#Families_paginate ul li.next")
 
     while nextPage == True:
@@ -67,35 +58,25 @@ with file:
         # currentPage = driver.find_element_by_xpath('//*[@id="Families_paginate"]/ul/li[@class="active"]/a')
         elm1 = driver.find_element_by_css_selector(
             "#Families_paginate")
-
         nextA = driver.find_element_by_css_selector("li.next a")
         nextLi = driver.find_element_by_css_selector("li.next")
-        # print(nextA)
-        # print(nextLi.get_attribute('class'))
-        # if nextA:
+
         if not 'disabled' in nextLi.get_attribute('class').split():
-            # nextPagePath = driver.find_element_by_xpath(
-            #     '//*[@id="Families_paginate"]/ul/li[@class="next"]/a')
             nextA.click()
             time.sleep(3)
 
             soup = BeautifulSoup(driver.page_source, 'lxml')
-
             dataTable = soup.find(id="Families")
-
             thead = dataTable.find_all("thead")
             tbody = dataTable.find_all("tbody")
-
             titleCols = thead[0].find_all("tr")[0].find_all('th')
             titles = [ele.text.strip() for ele in titleCols]
-
             dataRows = tbody[0].find_all("tr")
             for dataRow in dataRows:
                 dataCols = dataRow.find_all('td')
                 dataCols = [ele.text.strip() for ele in dataCols]
                 writer.writerow(dataCols)
-                # print(dataCols)
-            # print(len(elm1))
+
             print("Page " + str(page) + ":Data Fetched")
             page = page + 1
         else:
@@ -104,41 +85,3 @@ with file:
             break
 
 driver.close()
-
-
-# pagination = driver.find_elements_by_id("Families_paginate")
-
-
-# table = driver.find_element_by_id("Families").get_attribute('outerHTML')
-# data = pd.read_html(table)
-# print(data)
-# close browser after our manipulations
-# driver.close()
-
-# browser = driver.PhantomJS()
-# browser.get(URL)
-# html = browser.page_source
-# soup = BeautifulSoup(html, 'html.parser"')
-# a = soup.find(id="Families")
-
-# print(a)
-
-
-# page = requests.get(URL)
-# soup = BeautifulSoup(page.content, "html.parser")
-# dataTable = soup.find(id="Families")
-
-# thead = dataTable.find_all("thead")
-# tbody = dataTable.find_all("tbody")
-# print(tbody)
-# titleCols = thead[0].find_all("tr")[0].find_all('th')
-# titles = [ele.text.strip() for ele in titleCols]
-
-# dataRows = tbody[0].find_all("tr")
-# print(dataRows)
-
-# for dataRow in dataRows:
-#     dataCols = dataRow.find_all('td')
-#     dataCols = [ele.text.strip() for ele in dataCols]
-
-#     print(dataCols)
